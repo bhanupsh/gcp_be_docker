1ab3b780e825d05f660a503b51a053586cd25e69
bhanupsh <chbprakash.js@gmail.com>
2026-03-21 18:49:38 +0530

    first commit docker

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import commentsModel from "./models/comments.model.js";
+
+const app = express();
+
+await connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());   
+
+// default route
+app.get("/", (req, res) => {
+  res.send("Welcome to the Task On GCP - DOCKER .");
+});
+
+app.get("/api/comments", async (req, res) => {
+  try {
+    const comments = await commentsModel.find().limit(20).sort({ createdAt: -1 });
+    res.status(200).json(comments);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+export default app;
+
+
---
+++ server.js
@@ -0,0 +1,11 @@
+
+import { configDotenv } from "dotenv";
+configDotenv();
+
+import app from "./app.js";
+
+const PORT = process.env.PORT || 8080;  
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
