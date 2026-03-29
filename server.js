7fdd929f1badd8dfc9290488e6d745d62a9db2c7
bhanupsh <chbprakash.js@gmail.com>
2026-03-29 13:52:17 +0530

    cloud run issue

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-
 import { configDotenv } from "dotenv";
 configDotenv();
 
 import app from "./app.js";
 
-const PORT = process.env.PORT || 8080;  
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
